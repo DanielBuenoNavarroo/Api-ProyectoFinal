265b1376125494a809d3323c1e1d3bb4e97643bc
DanielBuenoNavarroo <danbue419@alu.centroafuera.es>
2024-04-26 10:53:18 +0200

    added email validator

--- src/api/models/user.model.js
+++ src/api/models/user.model.js
@@ -13,6 +13,12 @@ const userSchema = new mongoose.Schema(
       required: true,
       trim: true,
       unique: true,
+      validate: {
+        validator: function(email) {
+          return /^\w+([.-_+]?\w+)*@\w+([.-]?\w+)*(\.\w{2,10})+$/.test(email);
+        },
+        message: 'No es una dirección de correo electrónico válida.'
+      }
     },
     password: {
       type: String,
